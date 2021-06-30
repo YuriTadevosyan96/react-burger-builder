dfa589231bb4c2c04c1039184a0eba140b94c9fb
YuriTadevosyan96 <78906093+YuriTadevosyan96@users.noreply.github.com>
2021-06-30 22:06:35 +0300

    feat: add main structural components with styling

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import Burger from '../../components/Burger/Burger';
+
+export default class BurgerBuilder extends Component {
+  render() {
+    return (
+      <>
+        <Burger />
+        <div>Build Controls</div>
+      </>
+    );
+  }
+}
