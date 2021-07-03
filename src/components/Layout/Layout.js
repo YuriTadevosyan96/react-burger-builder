7e6131c875d9313f0a08677da125fe5256e28237
YuriTadevosyan96 <78906093+YuriTadevosyan96@users.noreply.github.com>
2021-07-03 20:35:37 +0300

    feat: add toolbar sidedrawer with toggle

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,38 @@
 import React from 'react';
 
 import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-function Layout(props) {
-  return (
-    <>
-      <div>Toolbar, SideDrawer, Backdrop</div>
-      <main className={classes.Content}>{props.children}</main>
-    </>
-  );
+class Layout extends React.Component {
+  state = {
+    showSideDrawer: false,
+  };
+
+  sideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerOpenHandler = () => {
+    this.setState({ showSideDrawer: true });
+  };
+
+  sideDrawerToggleHandler = () => {
+    this.setState((state) => ({ showSideDrawer: !state.showSideDrawer }));
+  };
+
+  render() {
+    return (
+      <>
+        <Toolbar
+          sideDrawerToggle={this.sideDrawerToggleHandler}
+          opened={this.sideDrawerOpenHandler}
+        />
+        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+        <main className={classes.Content}>{this.props.children}</main>
+      </>
+    );
+  }
 }
 
 export default Layout;
